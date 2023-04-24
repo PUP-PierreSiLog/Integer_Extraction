8ab57d5511af71fc84ecf44be4816a74d46c9f39
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-24 21:50:38 +0800

    Created a command that reads a source file. Note: Error occurs for the numbers.txt cannot be found.

--- Integer_Extraction.py
+++ Integer_Extraction.py
@@ -1,4 +1,7 @@
 #Reads a text file named numbers.txt
-#Identifies whether the numbers are odd or even
+with open("numbers.txt") as source:
+    #Identifies whether the numbers are odd or even
+    for line in source:
+        print(line.strip())
 #Creates a text file for the odd numbers
 #Creates a text file for the even numbers
