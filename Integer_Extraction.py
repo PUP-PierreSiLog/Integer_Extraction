1bda18b7a88a7850dab52955a79dc798e5466b4c
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-24 23:08:53 +0800

    Created a separator for the odd and even numbers.

--- Integer_Extraction.py
+++ Integer_Extraction.py
@@ -2,6 +2,9 @@
 with open("D:\[CMPE103] OOP\Integer_Extraction\\numbers.txt") as source:
     #Identifies whether the numbers are odd or even
     for line in source:
-        print(line.strip())
+        strtoint=int(line)
+        #Determines if the numbers present are odd or even
+        if strtoint%2==0:
+        else:
 #Creates a text file for the odd numbers
 #Creates a text file for the even numbers
