9fa745ceb48de29cc3489e598986f2aeba72b2b9
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-25 12:50:07 +0800

    Sorted the odd and even numbers

--- Integer_Extraction.py
+++ Integer_Extraction.py
@@ -1,10 +1,10 @@
-#Reads a text file named numbers.txt
-with open("D:\[CMPE103] OOP\Integer_Extraction\\numbers.txt") as source:
+#Reads a text file named numbers.txt, and create a text file for the odd and even integers
+with open("D:\[CMPE103] OOP\Integer_Extraction\\numbers.txt") as source, open("D:\[CMPE103] OOP\Integer_Extraction\\Odd.txt", "a") as odd_output, open("D:\[CMPE103] OOP\Integer_Extraction\\even.txt", "a") as even_output:
     #Identifies whether the numbers are odd or even
     for line in source:
         strtoint=int(line)
         #Determines if the numbers present are odd or even
         if strtoint%2==0:
+            even_output.write(strtoint+"\n")
         else:
-#Creates a text file for the odd numbers
-#Creates a text file for the even numbers
+            odd_output.write(strtoint+"\n")
